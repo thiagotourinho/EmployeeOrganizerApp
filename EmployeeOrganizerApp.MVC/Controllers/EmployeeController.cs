3fd9ca43bef353368e1b275b6af3b83787da1145
thiagotourinho <tftc05@gmail.com>
2023-05-04 10:05:49 -0300

    final implementations

--- EmployeeOrganizerApp.MVC/Controllers/EmployeeController.cs
+++ EmployeeOrganizerApp.MVC/Controllers/EmployeeController.cs
@@ -17,19 +17,39 @@ namespace EmployeeOrganizerApp.MVC.Controllers
                
         public ActionResult AddOrEdit(int id = 0)
         {
+            if (id == 0)
+                return View(new mvcEmployeeModel());
+            else {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("api/Employee/"+ id.ToString()).Result;
 
-            return View(new mvcEmployeeModel());
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
         }
 
         [HttpPost]
         public ActionResult AddOrEdit(mvcEmployeeModel employee)
         {
-            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", employee).Result;
 
+            if (employee.EmployeeID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Employee", employee).Result;
+                TempData["SucessMessage"] = "Saved Sucessfully";
+            }
+            else {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Employee/" + employee.EmployeeID, employee).Result;
+                TempData["SucessMessage"] = "Updated Sucessfully";
+            }
 
             return RedirectToAction("Index");
         }
 
+        [HttpDelete]
+        public ActionResult Delete(int id) {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("api/Employee/" + id.ToString()).Result;
+            TempData["SucessMessage"] = "Deleted Sucessfully";
+
+            return RedirectToAction("Index");
+        }
 
     }
 }
